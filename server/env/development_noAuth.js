9500ea090290121c8edcb8751a764199aa004927
Nicholas VanVoorthuysen <nicholas.vanvoorthuysen@gmail.com>
2017-04-18 12:09:05 -0400

    deleted extraneous login page, only use modal

---
+++ server/env/development_noAuth.js
@@ -0,0 +1,36 @@
+// var goauth = require ('../../goauth.js');
+// var gClientId = goauth.GoogleClientID;
+// var gClientSecret = goauth.GoogleClientSecret;
+// var gCallbackURL = goauth.GoogleCallbackURL;
+//
+//
+// var ghauth = require ('../../githubauth.js');
+// var ghClientId = ghauth.GithubClientID;
+// var ghClientSecret = ghauth.GithubClientSecret;
+// var ghCallbackURL = ghauth.GithubCallbackURL;
+
+module.exports = {
+  "DATABASE_URI": "mongodb://localhost:27017/layout-lasso",
+  "SESSION_SECRET": "Optimus Prime is my real dad"
+};
+
+// "TWITTER": {
+//   "consumerKey": "INSERT_TWITTER_CONSUMER_KEY_HERE",
+//   "consumerSecret": "INSERT_TWITTER_CONSUMER_SECRET_HERE",
+//   "callbackUrl": "INSERT_TWITTER_CALLBACK_HERE"
+// },
+// "FACEBOOK": {
+//   "clientID": "INSERT_FACEBOOK_CLIENTID_HERE",
+//   "clientSecret": "INSERT_FACEBOOK_CLIENT_SECRET_HERE",
+//   "callbackURL": "INSERT_FACEBOOK_CALLBACK_HERE"
+// },
+// "GOOGLE": {
+//   "clientID": gClientId,
+//   "clientSecret": gClientSecret,
+//   "callbackURL": gCallbackURL
+// },
+//   "GITHUB": {
+//   "clientID": ghClientId,
+//   "clientSecret": ghClientSecret,
+//   "callbackURL": ghCallbackURL
+// }
